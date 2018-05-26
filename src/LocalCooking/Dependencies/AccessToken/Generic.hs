aa3b75cd421103849267733ec16ed3cd1cb6095d
Athan Clark <athan.clark@gmail.com>
2018-05-26 08:33:22 -0600

    initial auth token dep, with social login

--- src/LocalCooking/Dependencies/AccessToken/Generic.hs
+++ src/LocalCooking/Dependencies/AccessToken/Generic.hs
@@ -21,6 +21,8 @@ Portability: GHC
 
 module LocalCooking.Dependencies.AccessToken.Generic where
 
+import LocalCooking.Function.System.AccessToken
+  (AccessTokenContext (..), revokeAccess, lookupAccess)
 import LocalCooking.Common.AccessToken (AccessToken, genAccessToken)
 import Web.Dependencies.Sparrow.Types (Server, ServerContinue (..), ServerReturn (..), ServerArgs (..))
 
@@ -62,74 +64,6 @@ class AccessTokenDeltaOut deltaOut where
   makeRevoke :: deltaOut
 
 
--- * Context
-
--- | Global context for an access token - expiring mapping with concurrent locks
-data AccessTokenContext k a = AccessTokenContext
-  { accessTokenContextSubject :: TimeMap k a
-  , accessTokenContextExpire  :: TMapMVar k () -- ^ Expiration lock
-  }
-
-
-
-newAccessTokenContext :: Newtype k AccessToken => STM (AccessTokenContext k a)
-newAccessTokenContext = AccessTokenContext <$> newTimeMap <*> newTMapMVar
-
-
--- * Utilities
-
-revokeAccess :: Hashable k
-             => Eq k
-             => AccessTokenContext k a
-             -> k
-             -> STM ()
-revokeAccess AccessTokenContext{..} accessToken = do
-  TimeMap.delete accessToken accessTokenContextSubject
-  TMapMVar.insert accessTokenContextExpire accessToken ()
-
-
-insertAccess :: Hashable k
-             => Eq k
-             => Newtype k AccessToken
-             => AccessTokenContext k a
-             -> a
-             -> IO k
-insertAccess AccessTokenContext{accessTokenContextSubject} x = do
-  accessToken <- pack <$> genAccessToken
-  TimeMap.insert accessToken x accessTokenContextSubject
-  pure accessToken
-
-
-
-lookupAccess :: Hashable k
-             => Eq k
-             => AccessTokenContext k a
-             -> k
-             -> IO (Maybe a)
-lookupAccess AccessTokenContext{accessTokenContextSubject} accessToken = do
-  mSubj <- atomically (TimeMap.lookup accessToken accessTokenContextSubject)
-  TimeMap.touch accessToken accessTokenContextSubject
-  pure mSubj
-
-
-
--- * Global Functions
-
--- | Independently forked thread that drains the timemap and invokes the concurrency locks when
--- tokens expire.
-expireThread  :: Hashable k
-              => Eq k
-              => NominalDiffTime
-              -> AccessTokenContext k a
-              -> IO ()
-expireThread expiration AccessTokenContext{..} = forever $ do
-  xs <- TimeMap.takeFromNow expiration accessTokenContextSubject
-  forM_ xs $ \(authToken,_) ->
-    atomically (TMapMVar.insert accessTokenContextExpire authToken ())
-  threadDelay $
-    let second = 10 ^ 6
-        minute = second * 60
-    in  minute
 
 
 -- | Create a Sparrow dependency for the access token implementation
